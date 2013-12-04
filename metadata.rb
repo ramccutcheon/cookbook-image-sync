e138eb1291e89a741215bc27b8509e394a15ed91
alop <alopgeek@gmail.com>
2013-12-04 14:50:03 -0800

    changed secret_key to enc dbag

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "alopgeek@gmail.com"
 license           "Apache 2.0"
 description       "Glance image-cache sync"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.0.1"
+version           "0.0.2"
 recipe            "image-sync::default", "Installs/configures btsync for glance"
 
 %w{ debian ubuntu }.each do |os|
